420f07166dc2718266c14be076a17d534c5757cc
Gaetan <gaetan.gelineau@gmail.com>
2024-11-16 18:28:20 +0100

    feat: setup main and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SearchModule } from './search/search.module';
+import { ConfigModule } from '@nestjs/config';
+import { loadConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [SearchModule, ConfigModule.forRoot({ load: [loadConfig] })],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
+
+  const configService = app.get(ConfigService<Config, true>);
+  const port = configService.get('port', { infer: true });
+
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
+
 bootstrap();
